c5d9c63c4703fb7cd71b0e0f2732132951370d1e
Patrick Arminio <patrick.arminio@gmail.com>
2021-04-12 23:20:32 +0100

    Update frontend

--- frontend/helpers/graphql.ts
+++ frontend/helpers/graphql.ts
@@ -1,14 +1,20 @@
 import { SubscriptionClient } from "subscriptions-transport-ws";
 import { Client, defaultExchanges, subscriptionExchange } from "urql";
 
+const WS_URL = "wss://strawberry-beats.onrender.com/graphql";
+const API_URL = "https://strawberry-beats.onrender.com/graphql";
+
+// const WS_URL = "ws://localhost:5000/graphql";
+// const API_URL = "http://localhost:5000/graphql";
+
 const subscriptionClient = process.browser
-  ? new SubscriptionClient("wss://strawberry-beats.onrender.com/graphql", {
+  ? new SubscriptionClient(WS_URL, {
       reconnect: true,
     })
   : null;
 
 export const client = new Client({
-  url: "https://strawberry-beats.onrender.com/graphql",
+  url: API_URL,
   exchanges: [
     ...defaultExchanges,
     subscriptionExchange({
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,52 +1,55 @@
 import { useMutation, useSubscription } from "urql";
 import useSound from "use-sound";
+import { SpriteMap } from "use-sound/dist/types";
 
 import Head from "next/head";
 
 const newMessages = `
-  subscription OnAlpaca {
-    onAlpaca {
-      x
-      y
+  subscription OnSound {
+    onSound {
+      name
     }
   }
 `;
 
 const playSound = `
-  mutation PlaySound {
-    playSound
+  mutation PlaySound($name: String!) {
+    playSound(name: $name)
   }
 `;
 
-const PlayButton = () => {
+const sprite = {
+  kick: [0, 350],
+  hihat: [374, 160],
+  snare: [666, 290],
+  cowbell: [968, 200],
+} as SpriteMap;
+
+const PlayButton = ({ name }) => {
   const [_, play] = useMutation(playSound);
 
   return (
     <button
+      className="p-10 border border-gray-500"
       onClick={() => {
-        play();
+        play({ name });
       }}
     >
-      Play a sound
+      {name}
     </button>
   );
 };
 
 const Sounds = () => {
   const [play] = useSound("/drums.mp3", {
-    sprite: {
-      kick: [0, 350],
-      hihat: [374, 160],
-      snare: [666, 290],
-      cowbell: [968, 200],
-    },
+    sprite,
   });
 
   const [{ data }] = useSubscription(
     { query: newMessages },
     (d, response: any) => {
       play({
-        id: ["kick", "hihat", "snare", "cowbell"][response.onAlpaca.x % 4],
+        id: response.onSound.name,
       });
 
       console.log("got sound");
@@ -66,12 +69,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Alpaca</title>
+        <title>Strawberry beats</title>
       </Head>
       <div className="w-screen h-screen border-4 relative border-green-500 bg-green-200 overflow-hidden">
         {process.browser && <Sounds />}
 
-        <PlayButton />
+        {Object.keys(sprite).map((name) => (
+          <PlayButton key={name} name={name} />
+        ))}
       </div>
     </>
   );
