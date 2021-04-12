7e622a0adc0d8472d891a2b77615511bee46e22a
Patrick Arminio <patrick.arminio@gmail.com>
2021-04-12 21:29:32 +0100

    Add cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import dataclasses
 from typing import Optional, Union
 
+from starlette.middleware import Middleware
+
 from broadcaster import Broadcast
 from starlette.applications import Starlette
 from starlette.requests import Request
@@ -8,6 +10,7 @@ from starlette.responses import Response
 from starlette.routing import Route, WebSocketRoute
 from starlette.websockets import WebSocket
 from strawberry.asgi import GraphQL
+from starlette.middleware.cors import CORSMiddleware
 
 from api.schema import schema
 
@@ -35,9 +38,11 @@ routes = [
     WebSocketRoute("/graphql", graphql_app),
 ]
 
+middleware = [Middleware(CORSMiddleware, allow_origins=["*"])]
 
 app = Starlette(
     routes=routes,
+    middleware=middleware,
     on_startup=[broadcast.connect],
     on_shutdown=[broadcast.disconnect],
 )
