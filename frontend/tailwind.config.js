584e98c87eb6beb8a63c07c8edabdde3ffb6c402
Patrick Arminio <patrick.arminio@gmail.com>
2021-04-14 21:19:12 +0100

    Update config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./pages/**/*.ts", "./components/**/*.ts"],
+  purge: [
+    "./pages/**/*.ts",
+    "./components/**/*.ts",
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
